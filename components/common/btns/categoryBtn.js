679f6ed6f70f8d04123011a13a5935c9b6449d72
Tsai-33 <cckkange@gmail.com>
2025-11-13 13:21:27 +0800

    feat:上方nav、首頁、樣式調整

--- components/common/btns/categoryBtn.js
+++ components/common/btns/categoryBtn.js
@@ -3,80 +3,29 @@ import clsx from "clsx";
 
 export default function CategoryBtn({ icon, text, variant, svgPath }) {
   const base = `
-    w-44 h-40
-    rounded-4xl 
-    text-lg
+    w-57.5 h-57.5
+    rounded-full 
+    border border-white
     flex justify-center items-center
     cursor-pointer 
     transition-colors duration-500 ease-in-out
-    backdrop-blur-[12px]
-    shadow-[0px_1px_8px_0px_#0000001A]
-    shadow-[0px_0px_2px_0px_#0000001A]
-    inset-shadow-[0px_0px_8px_0px_#F2F2F2]
-    inset-shadow-[0px_0px_0px_1px_#A6A6A6]
-    inset-shadow-[-2px_-2px_0.5px_-2px_#262626]
-    inset-shadow-[2px_2px_0.5px_-2px_#262626]
-    inset-shadow-[3px_3px_0.5px_-3.5px_#FFFFFF]
+    shadow-[0px_4px_4px_0px_#00000040]
+    shadow-[inset_0px_4px_4px_0px_#FFFFFF80]
     `;
 
   const variants = {
-    skyBlue: {
+    darkGreen: {
       btn: `
-      bg-[#DAE5EF] 
-      text-[#2D5780]
-      hover:text-[#4A9BC8]
-      focus:bg-[#CACACA]
-      focus:hover:bg-[#CACACA]
-      focus:hover:text-[#2D5780]
+        bg-[var(--primary-color)] 
+        text-[#FFFFFF]
+        hover:bg-[var(--green-vivid)]
+        diabled:bg-[var(--green-pale)]
+        diabled:text-[var(----primary-color)]
       `,
       text: `
-      text-[#6BB0E1]
-      [text-shadow:_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780,_0_0_3px_#2D5780]
-      `,
-    },
-    red: {
-      btn: `
-      bg-[#FFEAEE] 
-      text-[#E5495F]
-      hover:text-[#E86B7D]
-      focus:bg-[#FF8294]
-      focus:text-[#CACACA]
-      focus:hover:bg-[#FF8294]
-      focus:hover:text-[#CACACA]
-      `,
-      text: `
-      text-white
-      [text-shadow:_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D,_0_0_3px_#E86B7D]
-      `,
-    },
-    purple: {
-      btn: `
-      bg-[#F9E9FF] 
-      text-[#926BC5]
-      hover:text-[#C4B0DF]
-      focus:bg-[#A982DC]
-      focus:text-[#926BC5]
-      focus:hover:bg-[#A982DC]
-      focus:hover:text-[#926BC5]
-      `,
-      text: `
-      text-white
-      [text-shadow:_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF,_0_0_3px_#9747FF]
-      `,
-    },
-    orange: {
-      btn: `
-      bg-[#FFEBDB] 
-      text-[#E88121]
-      hover:text-[#FFA629]
-      focus:bg-[#FFAF17]
-      focus:text-[#000000]
-      focus:hover:bg-[#FFAF17]
-      focus:hover:text-[#000000]
-      `,
-      text: `
-      text-[#FFD79F]
-      [text-shadow:_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121,_0_0_3px_#E88121]
+        text-[length:var(--font-size-6xl)]
+        text-[var(--green-deep)]
+        font-bold
       `,
     },
   };
@@ -85,7 +34,7 @@ export default function CategoryBtn({ icon, text, variant, svgPath }) {
       <div className="flex flex-col items-center gap-5">
         <button className={clsx(base, variants[variant]?.btn)}>
           {svgPath ? (
-            <img 
+            <img
               src={svgPath}
               alt={text}
               className="w-[120px] h-[120px] object-contain"
