2ac5d783fc88e948b36099e8eeb2107ed5b24fa9
Tsai-33 <cckkange@gmail.com>
2025-11-18 17:01:59 +0800

    棧板

--- pages/test.js
+++ pages/test.js
@@ -4,16 +4,15 @@ import React from "react";
 export default function Test() {
   return (
     <>
-      {/* 站點切換按鈕 */}
-      <div className="w-full flex justify-between">
-        {stations.map((station) => (
-          <ActionBtn
-            text={station}
-            variant="green"
-            disabled={currentStation === station ? true : false}
-            onClick={() => handleSwitchStation(station)}
-          />
-        ))}
+      {/* 貨架 */}
+      <div className="w-250 flex flex-col gap-0.5">
+        <div className="w-full h-69 bg-[#DCB692] rounded-3xl p-2">
+          <div></div>
+          <div></div>
+        </div>
+        <div
+          className="w-full h-20 border-black border-x-20 border-t-20"
+          border></div>
       </div>
     </>
   );
