5f31f15189e6a7e717986e364462edc60a0b9920
milu0925u <milu17y@gmail.com>
2026-02-12 14:25:16 +0800

    dddddddddddd

--- components/inbound/inboundContext.js
+++ components/inbound/inboundContext.js
@@ -458,6 +458,7 @@ export default function InboundContext({ barCodeRef, setLoading }) {
           dispatch(setInbound({ selected: selected, station: res?.data?.data?.station }));
         } else if (shelf?.PALLET_NO === "") {
           toast.error("沒有指定棧板請自行選擇上架貨物");
+          console.log('V')
         } else {
           toast.error("不是此棧板的貨物");
         }
